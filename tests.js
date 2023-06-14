875b07147211c56ba777c80c4537b44f47d9ca42
Sandra Adey <sandraadeyinka@gmail.com>
2023-06-14 20:43:28 +0100

    added 2 more arrays to test

--- tests.js
+++ tests.js
@@ -4,5 +4,7 @@ const expect = chai.expect
 describe("getCelsius", () => {
     it('should covert fahrenheit to celsius for all values in an array', () =>{
       expect(getCelsius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5])
+      expect(getCelsius([-58, -22, -4, 14])).to.deep.equal([-50, -30, -20, -10])
+      expect(getCelsius([104, 122, 158, 176])).to.deep.equal([40, 50, 70, 80])
    })
 })
